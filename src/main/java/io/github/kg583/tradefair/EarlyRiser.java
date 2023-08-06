6dcf351d5711f6368ba80d3c63a28261a64e8378
KG <kgscience@hotmail.com>
2023-08-06 00:42:06 -0400

    Add new gossip types

--- src/main/java/io/github/kg583/tradefair/EarlyRiser.java
+++ src/main/java/io/github/kg583/tradefair/EarlyRiser.java
@@ -1,7 +1,28 @@
 package io.github.kg583.tradefair;
 
+import com.chocohead.mm.api.ClassTinkerers;
+import com.chocohead.mm.api.EnumAdder;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.MappingResolver;
+
 public class EarlyRiser implements Runnable {
+    public static MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
+
+    private void addGossipTypes() {
+        String gossipType = remapper.mapClassName("intermediary", "net.minecraft.class_4139");
+        EnumAdder builder =
+                ClassTinkerers.enumBuilder(gossipType, String.class, int.class, int.class, int.class, int.class);
+
+        builder.addEnum("CARPETS","carpets", 1, 30, 0, 1);
+        builder.addEnum("DECOR_BLOCKS","decor_blocks", 1, 25, 0, 1);
+        builder.addEnum("FLOWERS", "flowers", 1, 20, 0, 1);
+        builder.addEnum("GLASS", "glass", 1, 30, 0, 1);
+        builder.addEnum("LIGHTING","lighting", 2, 200, 0, 5);
+        builder.addEnum("SIGNAGE","signage", 1, 5, 0, 1);
+    }
+
     @Override
     public void run() {
+        addGossipTypes();
     }
 }
