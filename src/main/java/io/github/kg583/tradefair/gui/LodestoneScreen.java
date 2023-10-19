d5e985d616a6b8290b61d6d2aa0c245fd8f84d57
KG <kgscience@hotmail.com>
2023-10-18 21:56:42 -0400

    Baby

--- src/main/java/io/github/kg583/tradefair/gui/LodestoneScreen.java
+++ src/main/java/io/github/kg583/tradefair/gui/LodestoneScreen.java
@@ -3,6 +3,7 @@ package io.github.kg583.tradefair.gui;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
@@ -10,10 +11,8 @@ import net.minecraft.util.Identifier;
 import net.minecraft.village.VillagerProfession;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class LodestoneScreen extends HandledScreen<LodestoneScreenHandler> {
     private static final Identifier TEXTURE = new Identifier("minecraft", "textures/gui/book.png");
@@ -54,17 +53,40 @@ public class LodestoneScreen extends HandledScreen<LodestoneScreenHandler> {
 
     @Override
     protected void drawForeground(DrawContext context, int mouseX, int mouseY) {
-        context.drawText(this.textRenderer, this.title, 28, 0, 0, false);
-
+        int x = 28;
         int y = 0;
-        for (Map.Entry<VillagerProfession, List<Entity>> entry : this.employment.entrySet()) {
-            y += 16;
-            context.drawText(this.textRenderer, Text.of(String.format("%dx %s", entry.getValue().size(),
-                    StringUtils.capitalize(entry.getKey().id()))), 28, y, 0, false);
+
+        context.drawText(this.textRenderer, this.title, x, y, 0, false);
+
+        Map<String, Integer> employment =
+                this.employment.entrySet().stream().collect(Collectors.toMap((entry) -> entry.getKey().id(),
+                        (entry) -> entry.getValue().size()));
+
+        int unemployed = 0;
+        int baby = 0;
+        for (Entity villager : this.employment.getOrDefault(VillagerProfession.NONE, new ArrayList<>())) {
+            if (((VillagerEntity) villager).isBaby()) {
+                baby += 1;
+            } else {
+                unemployed += 1;
+            }
+        }
+
+        employment.put("baby", baby);
+        employment.put("unemployed", unemployed);
+        employment.remove("none");
+
+        for (Map.Entry<String, Integer> entry : employment.entrySet()) {
+            if (entry.getValue() > 0) {
+                y += 16;
+
+                context.drawText(this.textRenderer, Text.of(String.format("%dx %s", entry.getValue(),
+                        StringUtils.capitalize(entry.getKey()))), x, y, 0, false);
+            }
         }
 
         y += 32;
-        context.drawText(this.textRenderer, Text.of(String.format("Happiness — %.2f", this.happiness)), 28, y, 0,
+        context.drawText(this.textRenderer, Text.of(String.format("Happiness — %.2f", this.happiness)), x, y, 0,
                 false);
     }
 }
